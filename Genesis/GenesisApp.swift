d933360530eeb0485aa8ae9f75d38973ca0cedbd
Johnson Elangbam <elangbamjohnson@gmail.com>
2026-07-01 21:52:36 +0530

    Initial Genesis project

---
+++ Genesis/GenesisApp.swift
@@ -0,0 +1,17 @@
+//
+//  GenesisApp.swift
+//  Genesis
+//
+//  Created by Johnson Elangbam on 01/07/26.
+//
+
+import SwiftUI
+
+@main
+struct GenesisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
